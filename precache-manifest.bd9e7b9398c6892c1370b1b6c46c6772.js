2648d047cfef80b69c5ad524547463f7372a2781
Vlad Shum <shumanskyivlad@gmail.com>
2019-10-01 17:41:33 +0300

    Updates

---
+++ precache-manifest.bd9e7b9398c6892c1370b1b6c46c6772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d8f9ed0b38c93c66413",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.d734538f.js"
+  },
+  {
+    "revision": "d6e321341f925a08238a",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.00628694.chunk.js"
+  },
+  {
+    "revision": "6e8446a8e2d3fb9cd1b5",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.56d98a48.chunk.js"
+  },
+  {
+    "revision": "d6e321341f925a08238a",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.009764f0.chunk.css"
+  },
+  {
+    "revision": "6e8446a8e2d3fb9cd1b5",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.d6ef9ad8.chunk.css"
+  },
+  {
+    "revision": "ac854255f5019b8db5d30312bf7acc64",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  }
+];
